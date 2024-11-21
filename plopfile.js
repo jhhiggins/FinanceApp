6dbb46e64388dd97278d645e1f19ea8af91c9366
Jack Higgins <jhhiggins@outlook.com>
2024-11-21 17:46:01 -0500

    Changed SCSS to CSS in plopfile

--- plopfile.js
+++ plopfile.js
@@ -42,7 +42,7 @@ module.exports = (plop) => {
       },
       {
           type: 'add',
-          path: 'src/styles/{{pascalCase name}}.scss',
+          path: 'src/styles/{{pascalCase name}}.css',
           template: '',
       },
     ],
