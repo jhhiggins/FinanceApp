ba62eb8fbb1848ce096278948c55d51c49a496fd
Jack Higgins <jhhiggins@outlook.com>
2024-11-21 17:29:27 -0500

    Change SCSS to CSS to resolve ERRORS
    Did not have the correct imports to support SCSS files, reverting back to CSS resolves this issue w/o having to do much research

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import '../styles/App.css';
+import './styles/App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 
